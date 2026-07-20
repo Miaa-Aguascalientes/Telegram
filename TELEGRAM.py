a6bd9a4ac3e533aba44b1affe040bea9b894f75c
Miaa <pedroguillermomiaa@gmail.com>
2026-07-20 12:00:54 -0600

    Update TELEGRAM.py

--- TELEGRAM.py
+++ TELEGRAM.py
@@ -50,6 +50,7 @@ def convertir_a_hora(valor):
 col_h1, col_h2 = st.columns([1, 10])
 with col_h1: st.image("https://raw.githubusercontent.com/Miaa-Aguascalientes/Logos/38504978c8f77a4dac38ad476f74dbdee6af2cad/LogoMIAA.svg", width=150)
 with col_h2: st.markdown('<h1 class="custom-title">Sistema de Monitoreo</h1>', unsafe_allow_html=True)
+st.divider()    
 
 st.toggle("Activar envío de alertas a Telegram", key="alertas_activas") 
 
